6fb88601034db43278c4a1096f2fd26921bfa96b
fridenity0777 <fridenity0777@proton.me>
2023-05-14 14:03:05 +0700

    2023-05-14

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,20 +1,14 @@
 ## -- Aliases
 
 ## Better Defaults
-alias l="exa -ah --icons --color=always --group-directories-first"
 alias ls="exa -lah --icons --color=always --git --group-directories-first --color-scale"
-alias lss="exa -lahUumF@ --icons --color=always --git --group-directories-first --color-scale"
-alias lsss="exa -lahUumF@HSi --icons --color=always --git --group-directories-first --color-scale"
-alias lr="exa -Rlah --icons --color=always --git --group-directories-first --color-scale"
-alias lg="exa -a --icons --color=always --git --group-directories-first --color-scale"
-alias lt="exa -Tlah -L 3 --icons --color=always --git --group-directories-first --color-scale"
-alias l.="exa -ah --icons --color=always --group-directories-first | rg '^*\.'"
 
 alias diff="colordiff"
 alias egrep="egrep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias grep="grep --color=auto"
 alias black="black -S"
+alias man="batman"
 
 alias ct="column -t"
 alias df="df -hPT"
@@ -47,93 +41,108 @@ alias visudo="sudoedit /etc/sudoers"
 ## -- Abbreviations
 
 ## Shorthands
-abbr -a s 'sudo'
-abbr -a f 'hyfetch -b fastfetch'
-abbr -a pf 'pfetch'
-abbr -a nf 'neofetch'
-abbr -a ff 'fastfetch'
-abbr -a hf 'hyfetch'
-abbr -a py 'python'
-abbr -a ipy 'ipython'
-abbr -a rs 'evcxr'
-abbr -a irs 'irust'
-abbr -a swi 'swww img'
-abbr -a ht 'htop'
-abbr -a bt 'btop'
-abbr -a pk 'pkill'
+abbr l "lf"
+abbr s 'sudo'
+abbr f 'hyfetch'
+abbr pf 'pfetch'
+abbr nf 'neofetch'
+abbr ff 'fastfetch'
+abbr hf 'hyfetch'
+abbr py 'python'
+abbr ipy 'ipython'
+abbr rs 'evcxr'
+abbr irs 'irust'
+abbr swi 'swww img'
+abbr ht 'htop'
+abbr bt 'btop'
+abbr pk 'pkill'
+abbr ssv 'sudo sv'
+abbr Sv 'sudo sv'
 
 ## Utilities
-abbr -a d 'date +%F'
-abbr -a now 'date +"%T"'
-abbr -a nowtime 'now'
-abbr -a nowdate 'date +"%m-%d-%Y"'
+abbr d 'date +%F'
+abbr now 'date +"%T"'
+abbr nowtime 'now'
+abbr nowdate 'date +"%m-%d-%Y"'
 
-abbr -a grubup 'sudo update-grub'
-abbr -a fxpac 'sudo rm /var/lib/pacman/db.lck'
-abbr -a tarnow 'tar -acf'
-abbr -a untar 'tar -zxvf'
-abbr -a psmem 'ps auxf | sort -nr -k 4'
-abbr -a psmem10 'ps auxf | sort -nr -k 4 | head -10'
-abbr -a hw 'hwinfo --short'
-abbr -a big "expac -H M '%m\t%n' | sort -h | nl"
-abbr -a acqr "asciiquarium -t"
+abbr grubup 'sudo update-grub'
+abbr fxpac 'sudo rm /var/lib/pacman/db.lck'
+abbr tarnow 'tar -acf'
+abbr untar 'tar -zxvf'
+abbr psmem 'ps auxf | sort -nr -k 4'
+abbr psmem10 'ps auxf | sort -nr -k 4 | head -10'
+abbr hw 'hwinfo --short'
+abbr big "expac -H M '%m\t%n' | sort -h | nl"
+abbr acqr "asciiquarium -t"
+abbr hpc "hyprctl clients"
+
+abbr lss "ls -lahUumF@"
+abbr lsss "ls -lahUumF@HSin --changed --octal-permissions"
+abbr lr "ls -Rlah"
+abbr lg "exa -a --icons --color=always --git --group-directories-first --color-scale"
+abbr lt "ls -TL 3"
+abbr l. "exa -ah --icons --color=always --group-directories-first | rg '^*\.'"
 
 ## Directories
-abbr -a .c --position anywhere --set-cursor "$XDG_CONFIG_HOME/%"
-abbr -a .d --position anywhere --set-cursor "$XDG_DATA_HOME/%"
-abbr -a .l --position anywhere --set-cursor "$HOME/.local/lib/%"
-abbr -a .b --position anywhere --set-cursor "$HOME/.local/bin/%"
-abbr -a .h --position anywhere --set-cursor "/mnt/hdd/%"
-abbr -a .hp --position anywhere --set-cursor "/mnt/hdd/Pictures/%"
+abbr .c --position anywhere --set-cursor "$XDG_CONFIG_HOME/%"
+abbr .d --position anywhere --set-cursor "$XDG_DATA_HOME/%"
+abbr .l --position anywhere --set-cursor "$HOME/.local/lib/%"
+abbr .b --position anywhere --set-cursor "$HOME/.local/bin/%"
+abbr .h --position anywhere --set-cursor "/mnt/hdd/%"
+abbr .hp --position anywhere --set-cursor "/mnt/hdd/Pictures/%"
 
 ## Package Management
-abbr -a i "paru -S"
-abbr -a p "paru"
-abbr -a u "paru -Syu"
-abbr -a y "yay"
-abbr -a r "paru -R"
-abbr -a rr "paru -Rcns"
-abbr -a R "paru -Rcns"
+abbr i "paru -S"
+abbr p "paru"
+abbr u "paru -Syu"
+abbr y "yay"
+abbr r "paru -R"
+abbr rr "paru -Rcns"
+abbr R "paru -Rcns"
 
 ## Neovim
-abbr -a v "nvim"
-abbr -a V "sudoedit"
-abbr -a vv "sudoedit"
+abbr v "nvim"
+abbr V "sudoedit"
+abbr vv "sudoedit"
 
-abbr -a vb --set-cursor "nvim $HOME/.local/bin/%"
+abbr vb --set-cursor "nvim $HOME/.local/bin/%"
 
-abbr -a vc --set-cursor "nvim $XDG_CONFIG_HOME/%"
-abbr -a vcv "nvim $XDG_CONFIG_HOME/nvim/lua/user/init.lua"
-abbr -a vcf "nvim $XDG_CONFIG_HOME/fish/config.fish"
-abbr -a vcz "nvim $XDG_CONFIG_HOME/zsh/.zshrc"
-abbr -a vca "nvim $XDG_CONFIG_HOME/alacritty/..alacritty.yml && colorsubst"
-abbr -a vck "nvim $XDG_CONFIG_HOME/kitty/kitty.conf"
-abbr -a vckt "nvim $XDG_CONFIG_HOME/kitty/..theme.conf && colorsubst"
-abbr -a vch "nvim $XDG_CONFIG_HOME/hypr/hyprland.conf"
-abbr -a vchb "nvim $XDG_CONFIG_HOME/hypr/conf/binds.conf"
-abbr -a vchr "nvim $XDG_CONFIG_HOME/hypr/conf/rules.conf"
-abbr -a vchc "nvim $XDG_CONFIG_HOME/hypr/conf/chords.conf"
-abbr -a vcw "nvim $XDG_CONFIG_HOME/waybar/config && restart-waybar"
-abbr -a vcw2 "nvim $XDG_CONFIG_HOME/waybar/..style.css && colorsubst"
+abbr vc --set-cursor "nvim $XDG_CONFIG_HOME/%"
+abbr vcv "nvim $XDG_CONFIG_HOME/nvim/lua/user/init.lua"
+abbr vcf "nvim $XDG_CONFIG_HOME/fish/config.fish"
+abbr vcz "nvim $XDG_CONFIG_HOME/zsh/.zshrc"
+abbr vca "nvim $XDG_CONFIG_HOME/alacritty/..alacritty.yml && colorsubst"
+abbr vck "nvim $XDG_CONFIG_HOME/kitty/kitty.conf"
+abbr vckt "nvim $XDG_CONFIG_HOME/kitty/..theme.conf && colorsubst"
+abbr vch "nvim $XDG_CONFIG_HOME/hypr/hyprland.conf"
+abbr vchb "nvim $XDG_CONFIG_HOME/hypr/conf/binds.conf"
+abbr vchr "nvim $XDG_CONFIG_HOME/hypr/conf/rules.conf"
+abbr vchc "nvim $XDG_CONFIG_HOME/hypr/conf/chords.conf"
+abbr vcw "nvim $XDG_CONFIG_HOME/waybar/config && restart-waybar"
+abbr vcws "nvim $XDG_CONFIG_HOME/waybar/..style.css && colorsubst"
+abbr vcl "nvim $XDG_CONFIG_HOME/lf/lfrc"
+abbr vclc "nvim $XDG_CONFIG_HOME/lf/colors"
+abbr vcli "nvim $XDG_CONFIG_HOME/lf/icons"
+abbr vcs "nvim $XDG_CONFIG_HOME/starship.toml"
 
-abbr -a vA "nvim $HOME/.local/bin/autostart"
-abbr -a vC "nvim $XDG_DATA_HOME/colors.env && colorsubst"
-abbr -a vS "sudoedit /etc/sudoers"
+abbr vA "nvim $HOME/.local/bin/autostart"
+abbr vC "nvim $XDG_DATA_HOME/colors.env && colorsubst"
+abbr vS "sudoedit /etc/sudoers"
 
 ## Misc
-abbr -a :q 'exit'
-abbr -a :wq 'exit'
+abbr :q 'exit'
+abbr :wq 'exit'
 
 ## -- Environment Variables
 
 export SUDO_PROMPT=" "
 export EDITOR="nvim"
 
-
 ## -- Sources
 
 source /usr/share/doc/find-the-command/ftc.fish
 zoxide init fish | source
+eval (batpipe)
 
 ## -- Functions
 
